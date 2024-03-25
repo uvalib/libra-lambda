57bb7d9f7d01ef20823cb5d34581316944295c1f
Dave Goldstein <dpg3k@virginia.edu>
2024-03-25 09:41:19 -0400

    Flesh out indexing a bit more

--- libra-index/doc-render.go
+++ libra-index/doc-render.go
@@ -7,7 +7,9 @@ package main
 import (
 	"bytes"
 	"embed"
+	"fmt"
 	"github.com/uvalib/easystore/uvaeasystore"
+	"github.com/uvalib/libra-metadata"
 	"text/template"
 )
 
@@ -18,8 +20,15 @@ var templates embed.FS
 
 func docRender(cfg *Config, work uvaeasystore.EasyStoreObject) ([]byte, error) {
 
+	var templateFile string
+	switch work.Namespace() {
+	case libraEtdNamespace, libraOpenNamespace:
+		templateFile = fmt.Sprintf("templates/%s-solr-doc.template", work.Namespace())
+	default:
+		return nil, fmt.Errorf("unsupported namespace (%s)", work.Namespace())
+	}
+
 	// read the template
-	templateFile := "templates/solr-doc.template"
 	templateStr, err := templates.ReadFile(templateFile)
 	if err != nil {
 		return nil, err
@@ -31,22 +40,46 @@ func docRender(cfg *Config, work uvaeasystore.EasyStoreObject) ([]byte, error) {
 		return nil, err
 	}
 
-	type Work struct {
-		Id string // work identifier
-	}
-	type Attributes struct {
-		Doc Work
+	switch work.Namespace() {
+
+	case libraOpenNamespace:
+		return renderOpen(cfg, tmpl, work)
+
+		//case libraEtdNamespace:
+		//		return renderEtd(cfg, tmpl, work)
 	}
 
-	//	populate the work
-	doc := Work{
-		Id: work.Id(),
+	return nil, fmt.Errorf("unsupported namespace (%s)", work.Namespace())
+}
+
+func renderOpen(cfg *Config, tmpl *template.Template, work uvaeasystore.EasyStoreObject) ([]byte, error) {
+
+	type Attributes struct {
+		Doi     string // work DOI
+		Id      string // work identifier
+		PubYear string // publication year
+
+		Work librametadata.OAWork
+	}
+
+	// extract the metadata
+	md := work.Metadata()
+	pl, err := md.Payload()
+	if err != nil {
+		return nil, err
+	}
+	meta, err := librametadata.OAWorkFromBytes(pl)
+	if err != nil {
+		return nil, err
 	}
 
 	//	populate the attributes
-	//fields := work.Fields()
+	fields := work.Fields()
 	attribs := Attributes{
-		Doc: doc,
+		Work:    *meta,
+		Doi:     fields["doi"],
+		Id:      work.Id(),
+		PubYear: "YYYY", // TODO
 	}
 
 	// render the template
@@ -56,6 +89,7 @@ func docRender(cfg *Config, work uvaeasystore.EasyStoreObject) ([]byte, error) {
 		return nil, err
 	}
 
+	fmt.Printf("%s\n", renderedBuffer.String())
 	return renderedBuffer.Bytes(), nil
 }
 
