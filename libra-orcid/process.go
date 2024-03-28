6795213f6deaf2446e40db3b8a011052b910f2fa
Dave Goldstein <dpg3k@virginia.edu>
2024-03-28 08:28:17 -0400

    Added placeholder libra-orcid lambda

---
+++ libra-orcid/process.go
@@ -0,0 +1,55 @@
+//
+// main message processing
+//
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/uvalib/librabus-sdk/uvalibrabus"
+)
+
+// we treat libraETD and libraOpen events differently
+var libraEtdNamespace = "libraetd"
+var libraOpenNamespace = "libraopen"
+
+func process(messageId string, messageSrc string, rawMsg json.RawMessage) error {
+
+	// convert to librabus event
+	ev, err := uvalibrabus.MakeBusEvent(rawMsg)
+	if err != nil {
+		fmt.Printf("ERROR: unmarshaling libra bus event (%s)\n", err.Error())
+		return err
+	}
+
+	fmt.Printf("EVENT %s from:%s -> %s\n", messageId, messageSrc, ev.String())
+
+	// load configuration
+	cfg, err := loadConfiguration()
+	if err != nil {
+		return err
+	}
+
+	// easystore access
+	es, err := newEasystore(cfg)
+	if err != nil {
+		fmt.Printf("ERROR: creating easystore (%s)\n", err.Error())
+		return err
+	}
+
+	// important, cleanup properly
+	defer es.Close()
+
+	_, err = getEasystoreObject(es, ev.Namespace, ev.Identifier)
+	if err != nil {
+		fmt.Printf("ERROR: getting object ns/oid [%s/%s] (%s)\n", ev.Namespace, ev.Identifier, err.Error())
+		return err
+	}
+
+	return nil
+}
+
+//
+// end of file
+//
