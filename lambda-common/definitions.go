e9715d3d84499f7c9898c19322aad5924ca52772
Dave G <dpg3k@virginia.edu>
2025-06-18 13:42:33 -0400

    More error definitions

--- lambda-common/definitions.go
+++ lambda-common/definitions.go
@@ -8,6 +8,8 @@ import "fmt"
 
 // error definitions
 var ErrNoMetadata = fmt.Errorf("no metadata")
+var ErrUserNotFound = fmt.Errorf("user not found")
+var ErrEmailNotFound = fmt.Errorf("email not found")
 
 // namespace definition
 var libraEtdNamespace = "libraetd"
