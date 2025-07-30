20788c903abc1213247c993056f3656aea19ab1b
Dave G <dpg3k@virginia.edu>
2025-07-30 18:05:14 -0400

    Add libra-page-metrics migrations and supporting pipeline changes

---
+++ libra-page-metrics/migrations/20250730174833_page_metrics.down.sql
@@ -0,0 +1,14 @@
+--
+--
+--
+
+BEGIN;
+
+-- drop the table if it exists
+DROP TABLE IF EXISTS page_metrics;
+
+COMMIT;
+
+--
+-- end of file
+--
---
+++ libra-page-metrics/migrations/20250730174833_page_metrics.up.sql
@@ -0,0 +1,30 @@
+--
+--
+--
+
+BEGIN;
+
+-- create page_metrics table
+CREATE TABLE page_metrics (
+   id          serial PRIMARY KEY,
+   metric_type ENUM('download', 'view'),
+   namespace   VARCHAR( 32 ) NOT NULL DEFAULT '' ,
+   oid         VARCHAR( 64 ) NOT NULL DEFAULT '',
+   file_id     VARCHAR( 64 ) NOT NULL DEFAULT '',
+   source_ip   VARCHAR( 32 ) NOT NULL DEFAULT '' ,
+   referrer    VARCHAR( 255 ) NOT NULL DEFAULT '',
+   user_agent  VARCHAR( 32 ) NOT NULL DEFAULT '' ,
+   accept_lang VARCHAR( 32 ) NOT NULL DEFAULT '',
+
+   event_time timestamp with time zone NOT NULL,
+   created_at timestamp with time zone NOT NULL DEFAULT NOW()
+);
+
+-- create the namespace/oid key index
+CREATE INDEX page_metrics_key_idx ON page_metrics(namespace, oid);
+
+COMMIT;
+
+--
+-- end of file
+--
