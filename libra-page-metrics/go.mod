85438ad75a9a211bccd3b5b581480aa498c363eb
Dave G <dpg3k@virginia.edu>
2025-07-31 13:41:05 -0400

    Add simple libra-page-metrics lambda and supporting pipeline steps

---
+++ libra-page-metrics/go.mod
@@ -0,0 +1,29 @@
+module github.com/uvalib/libra-page-metrics
+
+go 1.22
+
+toolchain go1.24.2
+
+require (
+	github.com/aquilax/truncate v1.0.1
+	github.com/aws/aws-lambda-go v1.49.0
+	github.com/lib/pq v1.10.9
+	github.com/uvalib/librabus-sdk/uvalibrabus v0.0.0-20250729171533-58f2a52d4a53
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.37.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.30.2 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.18.2 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/cloudwatchevents v1.29.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.26.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.31.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.35.1 // indirect
+	github.com/aws/smithy-go v1.22.5 // indirect
+)
---
+++ libra-page-metrics/process.go
@@ -0,0 +1,122 @@
+//
+// main message processing
+//
+
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/aquilax/truncate"
+
+	_ "github.com/lib/pq"
+	"github.com/uvalib/librabus-sdk/uvalibrabus"
+)
+
+// keep the following in-sync with the schema defined in the migrations
+var maxSourceIpSize = 32
+var maxReferrerSize = 255
+var maxUserAgentSize = 32
+var maxAcceptLanguageSize = 32
+
+func process(messageId string, messageSrc string, rawMsg json.RawMessage) error {
+
+	// convert to librabus event
+	ev, err := uvalibrabus.MakeBusEvent(rawMsg)
+	if err != nil {
+		fmt.Printf("ERROR: unmarshaling libra bus event (%s)\n", err.Error())
+		return err
+	}
+
+	fmt.Printf("INFO: EVENT %s from %s -> %s\n", messageId, messageSrc, ev.String())
+
+	content, err := uvalibrabus.MakeContentEvent(ev.Detail)
+	if err != nil {
+		fmt.Printf("ERROR: unmarshaling libra content event (%s)\n", err.Error())
+		return err
+	}
+
+	fmt.Printf("INFO: Content %v\n", content)
+
+	// load configuration
+	cfg, err := loadConfiguration()
+	if err != nil {
+		return err
+	}
+
+	connectionStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
+		cfg.DbHost, cfg.DbPort, cfg.DbUser, cfg.DbPassword, cfg.DbName)
+
+	db, err := sql.Open("postgres", connectionStr)
+	if err != nil {
+		fmt.Printf("ERROR: unable to open database %s\n", err.Error())
+		return err
+	}
+	// cleanup
+	defer db.Close()
+
+	parsedEventTime, err := time.Parse(time.RFC3339, ev.EventTime)
+	if err != nil {
+		fmt.Printf("ERROR: unable to parse event time %s\n", err.Error())
+		return err
+	}
+
+	var metricType string
+	var fileId string
+	switch ev.EventName {
+	case uvalibrabus.EventContentView:
+		metricType = "view"
+		fileId = ""
+
+	case uvalibrabus.EventContentDownload:
+		metricType = "download"
+		fileId = "blablabla"
+
+	default:
+		fmt.Printf("INFO: uninteresting event, ignoring\n")
+		return nil
+	}
+
+	// endure we do not exceed any character limits
+	content.SourceIp = truncate.Truncate(content.SourceIp, maxSourceIpSize, "...", truncate.PositionEnd)
+	content.Referrer = truncate.Truncate(content.Referrer, maxReferrerSize, "...", truncate.PositionEnd)
+	content.UserAgent = truncate.Truncate(content.UserAgent, maxUserAgentSize, "...", truncate.PositionEnd)
+	content.AcceptLanguage = truncate.Truncate(content.AcceptLanguage, maxAcceptLanguageSize, "...", truncate.PositionEnd)
+
+	// insert into the database
+	result, err := db.Exec("INSERT INTO page_metrics (metric_type, namespace, oid, file_id, source_ip, referrer, user_agent, accept_lang, event_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+		metricType,
+		ev.Namespace,
+		ev.Identifier,
+		fileId,
+		content.SourceIp,
+		content.Referrer,
+		content.UserAgent,
+		content.AcceptLanguage,
+		parsedEventTime,
+	)
+
+	if err != nil {
+		fmt.Printf("ERROR: db insert %s", err)
+		return err
+	}
+
+	n, err := result.RowsAffected()
+	if err != nil {
+		fmt.Printf("ERROR: rows affected %s", err)
+		return err
+	}
+
+	fmt.Printf("INFO: Inserted %d row\n", n)
+
+	// log the happy news
+	fmt.Printf("INFO: EVENT %s from %s processed OK\n", messageId, messageSrc)
+	return nil
+}
+
+//
+// end of file
+//
